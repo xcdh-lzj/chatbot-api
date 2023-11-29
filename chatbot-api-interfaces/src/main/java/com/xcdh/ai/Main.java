62a36919b7a4a7db5db8c465f049b71c202cf841
xcdh <2735516933@qq.com>
2023-11-29 15:58:25 +0800

    初次提交
    Took 10 minutes

---
+++ chatbot-api-interfaces/src/main/java/com/xcdh/ai/Main.java
@@ -0,0 +1,12 @@
+package com.xcdh.ai;
+
+/**
+ * @Author: xcdh
+ * @Date: 2023/11/29 15:57
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
